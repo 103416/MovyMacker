9371c9d302fe3ec60185d18fac7ce54834455581
zekerniels <103421@glr.nl>
2026-05-27 10:36:45 +0200

    null

--- script/script.js
+++ script/script.js
@@ -5,19 +5,19 @@ alert(fileName)
 let i = 0
 let screens = ["./screen1.html","./screen2.html","./index.html","./screen3.html","./screen4.html"]
 if(fileName === "screen4.html"){
-    i = 5
-}
-if(fileName === "screen3.html"){
     i = 4
 }
-if(fileName === "index.html"){
+if(fileName === "screen3.html"){
     i = 3
 }
-if(fileName === "screen2.html"){
+if(fileName === "index.html"){
     i = 2
 }
-if(fileName === "screen1.html"){
+if(fileName === "screen2.html"){
     i = 1
+}
+if(fileName === "screen1.html"){
+    i = 0
 }
 
 
